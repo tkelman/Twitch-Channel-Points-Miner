5a7e20c0fb6b22f5b7dd827ab0dd759fd8eb475c
Tony Kelman <tony@kelman.net>
2026-05-02 13:45:15 -0700

    remove 30 second delay after coming online before watching is allowed

--- TwitchChannelPointsMiner/miner.go
+++ TwitchChannelPointsMiner/miner.go
@@ -725,7 +725,7 @@ func (m *Miner) pickStreamersToWatch(streamers []*entities.Streamer) []*entities
 		if s == nil || !s.IsOnline {
 			continue
 		}
-		if !s.OnlineAt.IsZero() && now.Sub(s.OnlineAt) < 30*time.Second {
+		if !s.OnlineAt.IsZero() && now.Sub(s.OnlineAt) < 0*time.Second {
 			continue
 		}
 		m.refreshStreamForPreference(s)
